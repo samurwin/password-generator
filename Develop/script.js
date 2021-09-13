bc9c22016e6e940fa50fd8774082cbfd327c0eb7
smudesign <urwinsamantha@gmail.com>
2021-09-12 22:07:06 -0400

    variables for the characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,7 @@
 // Assignment code here
+var lowerCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j","k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharacter = ["!", "'", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "/", "]", "^", "_", "`", "{", "}", "~"];
 
 // create a function to gather criteia for the password
 var passCriteria = function() {
